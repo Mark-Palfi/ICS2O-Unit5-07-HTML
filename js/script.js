666a7dac9646ae57b72a7af2efc09453c59e5c2e
Mark-Palfi <99292598+Mark-Palfi@users.noreply.github.com>
2022-11-02 15:14:34 +0000

    edits

--- js/script.js
+++ js/script.js
@@ -7,11 +7,15 @@
 /**
  * This function gets users street name and number and shows it back to user.
  */
-function enterClicked() {
+function enterClicked(){
   // input
   const highestDigit = document.getElementById("highestDigit").value
+
+  //math
+  total == highestDigit / 2 * (highestDigit + 1)
+  
   // output
   for (let i = 0; i < highestDigit; i++) {
-  document.getElementById("total").innerHTML = "The total is 15";
+  document.getElementById("total").innerHTML = "The total is" ;total
   }
 }
